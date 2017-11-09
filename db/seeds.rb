f5affc35e36df8e3f7aa1fee60e7ef1c620d6c85
Sandra Woznicka <sandra.woznicka@gmail.com>
2017-11-09 22:38:38 +0000

    D

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+url = "http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+doc = open(url)
+result = JSON.parse(doc.read)
+result["drinks"].each do |ingredient_hash|
+  # p ingredient_hash.values[0]
+  ingredient = Ingredient.new(name: ingredient_hash.values[0])
+  ingredient.save!
+end
